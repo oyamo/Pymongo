85008039f7a1a54439023c25b74c02cdddc703be
Oyamo Brian <34618315+oyamoh-brian@users.noreply.github.com>
2020-03-05 16:54:33 +0300

    Mongo Setup Script
    Mon

--- mongo-installer.sh
+++ mongo-installer.sh
@@ -2,6 +2,9 @@
 # This file sets up mongodb for python in your environment
 # Author: Oyamo Brian oyamo.xyz@protonmail.com
 # You are free to modify this file or submit complaints
+echo "Setting up Mongo db for your environment"
+echo "This installs the python module and the mongo server and starts the server"
+echo "It is going to take Quite some Time"
 python -m pip install pymongo
 #python3 -m pip install pymongo
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc |  apt-key add -
@@ -11,3 +14,4 @@ echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" |  tee
 apt-get update
 apt-get install -y mongodb-org
 service mongod start
+echo " Thank you for your Patience"
