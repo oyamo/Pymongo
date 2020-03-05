7ff8f80693bade1d82afd68ebacc072cfec82878
Oyamo Brian <34618315+oyamoh-brian@users.noreply.github.com>
2020-03-05 16:40:00 +0300

    The file implementing mongodb
    RUN

--- index.py
+++ index.py
@@ -2,7 +2,10 @@
 '''
   first run the mongo-installer.sh 
   make sure the server deamon is running 
-  check the status using --- service mongod status
+  check the status using 
+  service mongod status 
+  or
+  sudo service --status-all | grep postgres
   if it is not running, run --- service mongod start
  '''
  
@@ -28,3 +31,61 @@ print(myclient.list_database_names())
 student_details_collection = mydb['student-details']
 
 # Inserting Records into a collection
+'''
+  There are two methods that can be used
+  -insert_one(..) - Insert the dictionary parameter into the database
+  -insert_many(..) - Inserts a lists of dictionaries into the mongo db collection
+'''
+# Inserting Only one record
+random_student = {'Name' : 'Oyamo Brian', 'Age' : 34, 'Course' : 'Computer Science'}
+student_details_collection.insert_one(random_student)
+# Inserting Many Records
+my_friends = [
+              {'Name' : 'Peter Joel', 'Age' : 34, 'Course' : 'Computer Science'},
+              {'Name' : 'John Doe', 'Age' : 32 'Course' : 'Computer Science'},
+                {'Name' : 'Peter Matiko', 'Age' : 32 'Course' : 'Journalism'},
+              {'Name' : 'Eric Odinga', 'Age': 54 'Course' : 'Mechanical Engineering'}
+            ]
+student_details_collection.insert_many(my_friends)
+
+# Reading from Collections
+'''
+  To select data from a mongodb collection, we use find() and find_one() methods
+  - find() - Returns all occurances in the collection
+  - find_one() - Returns only one occurance in the collection
+  
+ '''
+all_records = student_details_collection.find()
+print(all_records) # Will be printed in a dictionary format
+
+one_record = student_details_collection.find_one()
+print(one_record)
+
+# Filtering Collections
+'''
+  First set a filter eg
+  When we want to get all computer Science Students from our collection
+  we say;
+'''
+my_filter = {'Course' : 'Computer Science'}
+comp_sci_students =  student_details_collection.find(my_filter)
+
+print(comp_sci_students)
+
+# Deleting Records 
+'''
+  We use delete_one() and delete_many() methods
+  - delete_one() deletes only one instance 
+  - delete_many() deletes all instances if the selected records
+'''
+age32_filter = {'Age': 32}
+student_details_collection.delete_many(age32_filter) # Guess What it deletes ; The records of students whose age is 32
+
+# Dropping a collection
+'''
+  Dropping a collection is deleting a whole collecton and all its records
+  we use drop() method
+  eg student_details_collection.drop()
+'''
+
+              
